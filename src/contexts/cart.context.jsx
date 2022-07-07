e1e227c19ee2800cf685cd3ff08d674d2f18a24f
Nebojša Jotanović <nebojsa.j@hotmail.rs>
2022-07-07 12:19:14 +0200

    created checkout page along with checkout items and checkout cart functionality

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -7,15 +7,35 @@ const addCartItem = (cartItems, productToAdd) => {
     if(cartItem) {
         return cartItems.map((cartItem) => 
             cartItem.id === productToAdd.id ? {
-                ...cartItem, quantity: cartItem.quantity +1
+                ...cartItem, quantity: cartItem.quantity + 1
             } : cartItem
         );
     };
-
     return [...cartItems,{...productToAdd, quantity: 1}];
-
-
 } 
+
+const removeCartItem = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(cartItem => cartItem.id == cartItemToRemove.id);
+
+    // check if quantity is 1, remove that item from the cart
+
+    if(existingCartItem.quantity == 1) {
+        return cartItems.filter(cartItem => cartItem.id != existingCartItem.id);
+    }
+
+    return cartItems.map((cartItem) => 
+    cartItem.id === cartItemToRemove.id ? {
+        ...cartItem, quantity: cartItem.quantity - 1
+    } : cartItem
+    
+);
+
+}
+
+const clearCartItem = (cartItems, cartItemToRemove) => {
+    return cartItems.filter(cartItem => cartItem.id != cartItemToRemove.id);
+}
+
 
 export const CartContext = createContext({
     isCartOpen: false,
@@ -23,6 +43,9 @@ export const CartContext = createContext({
     cartItems: [],
     addItemToCart: () => {},
     cartCount: 0,
+    cartTotal: 0,
+    removeItemFromCart: () => {},
+    clearItemFromCart: () => {}
 })
 
 // id
@@ -35,6 +58,7 @@ export const CartProvider = ({children}) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
     const [cartCount, setCartCount] = useState(0);
+    const [cartTotal, setCartTotal] = useState(0);
 
 
     useEffect( () => {
@@ -42,12 +66,29 @@ export const CartProvider = ({children}) => {
         setCartCount(newCartCount);
     },[cartItems]);
 
+    // one of best practices of useEffect, single responsibility.
+
+    useEffect( () => {
+        const newCartTotal = cartItems.reduce((total,cartItem) => total + (cartItem.price * cartItem.quantity), 0);
+        setCartTotal(newCartTotal);
+    },[cartItems]);
+
+
+
     const addItemToCart = (productToAdd) => {
         setCartItems(addCartItem(cartItems, productToAdd));
     }
 
+    const removeItemToCart = (product) => {
+        setCartItems(removeCartItem(cartItems, product));
+    }
 
-    const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount};
+    const clearItemFromCart = (product) => {
+        setCartItems(clearCartItem(cartItems, product));
+    }
+
+
+    const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount, removeItemToCart, clearItemFromCart, cartTotal};
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 }
