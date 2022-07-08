074a78031358f555b51fc54b4bef38b1ce21e837
Nebojša Jotanović <nebojsa.j@hotmail.rs>
2022-07-08 13:41:30 +0200

    Update

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,4 +1,10 @@
-import './navigation.styles.scss';
+import {
+  NavigationContainer,
+  LogoContainer,
+  NavLinks,
+  NavLink
+} from './navigation.styles';
+
 import { Fragment, useContext } from 'react';
 import { UserContext } from '../../contexts/user.context';
 import { CartContext } from '../../contexts/cart.context';
@@ -16,21 +22,21 @@ const Navigation = () => {
 
     return(
       <Fragment>
-        <div className='navigation'>
-            <Link className='logo-container' to='/'>
+        <NavigationContainer>
+            <LogoContainer to='/'>
                 <CrwnLogo className='logo' />
-            </Link>
-            <div className="nav-links-container">
-                <Link className='nav-link' to='/shop'>Shop</Link>
+            </LogoContainer>
+            <NavLinks>
+                <NavLink to='/shop'>Shop</NavLink>
                 {currentUser ? (
-                  <span className='nav-link' onClick={signOutUser}>Sign Out</span>)
-                  : (                <Link className='nav-link' to='/auth'>Sign In</Link>
+                  <NavLink as="span" onClick={signOutUser}>Sign Out</NavLink>)
+                  : (                <NavLink to='/auth'>Sign In</NavLink>
                   )
                  }
                 <CartIcon />
-            </div>
+            </NavLinks>
                 {isCartOpen && <CartDropdown />}
-        </div>
+        </NavigationContainer>
         <Outlet />
       </Fragment>
     )
